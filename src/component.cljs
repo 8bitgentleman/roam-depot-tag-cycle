1da2d71f25aa8cc187d83f4cad5c03bfcffdd52c
Matt Vogel <mainmoniker@googlemail.com>
2023-11-16 10:45:57 -0500

    fix: start cycle if no tag exists

--- src/component.cljs
+++ src/component.cljs
@@ -23,6 +23,7 @@
     (map :block/string)))
 
 (defn get-block-string [uid]
+  ;gets a block string from a uid
   (:block/string (rd/pull 
       '[:block/string 
         ] 
@@ -38,22 +39,23 @@
 
 (defn cycle-tags [uid parent-uid]
   (let [tags (get-options parent-uid)
-        block-string (get-block-string uid)]
+        block-string (get-block-string uid)
+        found-tag (atom false)] ; Create an atom to track if a tag was found and replaced
     (doseq [[index tag] (map-indexed vector tags)]
       (let [re (re-pattern (str "\\[\\[" tag "\\]\\](?=( |$))"))]
         (when (re-find re block-string)
+          (reset! found-tag true) ; Set the flag to true if a tag is found
           (let [rep-index (mod (inc index) (count tags))
                 new-block-string (clojure.string/replace block-string re (str "[[" (nth tags rep-index) "]]"))]
             (update-block-string uid new-block-string)))))
-  )
-)
+    (when-not @found-tag ; Check if no tag was found after the loop
+      (let [new-block-string (str block-string " #[[" (first tags) "]]")] ; Append the first tag
+        (update-block-string uid new-block-string))))) ; Update the block string
 
 
 (defn main [{:keys [block-uid]} & args]
   [:span.cycle-button { :class "cycle-button"
                      :draggable false
-                     :style {
-                             }
                      :on-click #(cycle-tags block-uid (get (first args) 1))}"↻"]
   
   )
